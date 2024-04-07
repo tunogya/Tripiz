c88ca709b656d94efea3e86d369741911d67871c
tunogya <20251942+tunogya@users.noreply.github.com>
2024-04-07 14:29:27 +0800

    update

--- app/newTask.tsx
+++ app/newTask.tsx
@@ -12,9 +12,16 @@ const NewTask = () => {
       className={"flex h-full bg-[#121212] px-3"}
     >
       <ScrollView className={'pt-4 space-y-6'}>
-        <Text className={"text-white font-medium"}>必做任务</Text>
-
-        <Text className={"text-white font-medium"}>选做任务</Text>
+        <View className={"space-y-2"}>
+          <Text className={"text-[#1ED760] font-bold"}>必做任务</Text>
+          <Text className={"text-white"}>{"  "}吃一次土耳其特色早餐</Text>
+          <Text className={"text-white"}>{"  "}完成一次出海拍摄</Text>
+        </View>
+        <View className={"space-y-2"}>
+          <Text className={"text-red-400 font-medium"}>选做任务</Text>
+          <Text className={"text-white"}>{"  "}去一户当地人家蹭饭</Text>
+          <Text className={"text-white"}>{"  "}不用翻译软件自己存活</Text>
+        </View>
       </ScrollView>
       <View className={"space-y-3"}>
         <Pressable className={"rounded-lg bg-[#272727] items-center"}>
