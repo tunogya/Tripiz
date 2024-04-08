5d64f24ac928c2915aaac306e4c6f2899d9f353a
tunogya <20251942+tunogya@users.noreply.github.com>
2024-04-09 00:22:24 +0800

    update

--- components/Task.tsx
+++ components/Task.tsx
@@ -12,28 +12,18 @@ const Task: FC<{
 
   return (
     <View className={"flex space-y-0.5 py-1.5"}>
-      <View className={"bg-[#292929] rounded-lg overflow-hidden"}>
-        <View className={"relative h-52 bg-[#292929] shadow-xl"}>
-          <Text className={"text-white truncate absolute left-3 top-3 font-semibold"}>
-            {task?.type === "main" ? "必做任务" : "选做任务"}
-          </Text>
-          <View className={"absolute w-full"}>
-            <View className={"flex items-center justify-center h-52"}>
-              <Ionicons name="image-outline" size={24} color="#A7A7A7" />
-            </View>
-          </View>
-        </View>
+      <View className={`rounded-lg overflow-hidden shadow-lg ${ task?.type === "main" ? "bg-[#181818]" : "border border-[#181818]"}`}>
         <View className={'p-3 space-y-3'}>
           <View className={"flex flex-row items-center justify-between"}>
-            <View className={"flex space-y-0"}>
-              <Text className={"text-white truncate text-lg font-bold"}>
+            <View className={"flex"}>
+              <Text className={"text-white truncate font-bold"}>
                 {task?.title}
               </Text>
-              {/*<Text className={"text-xs text-[#A7A7A7]"}>*/}
-              {/*  {task?.status === "SUCCESS" ? `任务完成时间：` : "任务未完成。"}*/}
-              {/*</Text>*/}
+              <Text className={"text-[#A7A7A7] font-semibold text-xs"}>
+                {task?.type === "main" ? "必做任务" : "选做任务"}
+              </Text>
             </View>
-            <Pressable className={`border ${task?.status === "SUCCESS" ? "border-white" : "border-[#A7A7A7]"} rounded-full px-3 py-1`}>
+            <Pressable className={`border ${task?.status === "SUCCESS" ? "border-white" : "border-[#A7A7A7]"} rounded-full px-3 py-1.5`}>
               <Text className={"text-white truncate text-xs font-semibold"}>
                 {task?.status === "SUCCESS" ? "已完成" : "完成"}
               </Text>
