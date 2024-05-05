272097e5c03e7c39c565c977b03426ca29f7b623
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-06 01:32:50 +0800

    update

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,18 +1,22 @@
 import { Pressable, View, Text } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { router } from "expo-router";
+import { BlurView } from "expo-blur";
 
 const AddButton = () => {
   const insets = useSafeAreaInsets();
+  const [openMore, setOpenMore] = useState(false);
 
-  return (
+  const normalView = () => (
     <View
-      className={"absolute flex flex-row items-center bg-white rounded-full shadow"}
+      className={
+        "absolute flex flex-row items-center bg-white rounded-full shadow"
+      }
       style={{
         right: 20,
-        bottom: insets.bottom + 80,
+        bottom: insets.bottom + 66,
       }}
     >
       <Pressable
@@ -31,11 +35,67 @@ const AddButton = () => {
         className={
           "items-center justify-center flex flex-row h-14 pl-3 pr-4 space-x-1"
         }
+        onPress={() => {
+          setOpenMore(true);
+        }}
       >
         <Ionicons name="chevron-up" size={24} color="#121212" />
       </Pressable>
     </View>
   );
+
+  const openMoreView = () => (
+    <View className={"absolute w-full h-full"}>
+      <BlurView intensity={100} tint="dark">
+        <View className={"relative w-full h-full"}>
+          <Pressable
+            onPress={() => {
+              setOpenMore(false);
+            }}
+            className={"absolute w-full h-full"}
+          ></Pressable>
+          <View
+            className={"absolute flex items-end space-y-3"}
+            style={{
+              right: 20,
+              bottom: insets.bottom + 66,
+            }}
+          >
+            <View className={"flex flex-row space-x-6 items-center"}>
+              <Text className={"text-white text-lg font-bold"}>
+                New thought
+              </Text>
+              <View className={"bg-[#242424] p-2 rounded-full m-0.5"}>
+                <MaterialCommunityIcons
+                  name="thought-bubble-outline"
+                  size={24}
+                  color="white"
+                />
+              </View>
+            </View>
+            <View className={"flex flex-row space-x-6 items-center"}>
+              <Text className={"text-white text-lg font-bold"}>New memory</Text>
+              <View className={"bg-[#242424] p-2 rounded-full m-0.5"}>
+                <Ionicons name="sunny-outline" size={24} color="white" />
+              </View>
+            </View>
+            <View className={"flex flex-row space-x-6 items-center"}>
+              <Text className={"text-white text-lg font-bold"}>New dream</Text>
+              <View className={"bg-white p-2.5 rounded-full"}>
+                <Ionicons name="moon-outline" size={24} color="black" />
+              </View>
+            </View>
+          </View>
+        </View>
+      </BlurView>
+    </View>
+  );
+
+  if (openMore) {
+    return openMoreView();
+  } else {
+    return normalView();
+  }
 };
 
 export default memo(AddButton);
