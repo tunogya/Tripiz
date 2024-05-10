bea38e18324e7be97003e2e002eaa3e61d194edb
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-10 21:59:27 +0800

    update

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -88,7 +88,7 @@ const Page = () => {
             <Pressable
               hitSlop={4}
               className={
-                "h-6 w-6 items-center justify-center bg-[#FFFFFF12] rounded-full mr-1.5"
+                "h-8 w-8 items-center justify-center bg-[#FFFFFF12] rounded-full mr-1.5"
               }
               onPress={() => {
                 setFilter("");
@@ -102,7 +102,7 @@ const Page = () => {
               <Pressable
                 hitSlop={4}
                 key={index}
-                className={`px-4 py-2 ${filter === item ? "bg-[#1DB954]" : "bg-[#FFFFFF12]"} rounded-full mx-1`}
+                className={`px-4 h-8 items-center justify-center ${filter === item ? "bg-[#1DB954]" : "bg-[#FFFFFF12]"} rounded-full mx-1`}
                 onPress={() => {
                   setFilter(item);
                 }}
