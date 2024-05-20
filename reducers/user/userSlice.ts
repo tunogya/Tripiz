8282df90ae0976c735f345b9e694876a6001b35c
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-20 18:44:24 +0800

    update

--- reducers/user/userSlice.ts
+++ reducers/user/userSlice.ts
@@ -7,16 +7,19 @@ export const slice = createSlice({
   name: "user",
   initialState: {
     address: "",
+    publicKey: "",
     privateKey: "",
   },
   reducers: {
     initialize: (state) => {
       const wallet = ethers.Wallet.createRandom();
       state.address = wallet.address
+      state.publicKey = wallet.publicKey
       state.privateKey = wallet.privateKey
     },
     destroy: (state) => {
       state.address = ""
+      state.publicKey = ""
       state.privateKey = ""
     }
   },
