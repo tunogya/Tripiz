2d859c2f2fab03b512a3867995f77b183844c960
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-22 11:30:06 +0800

    update

--- components/LibraryShowItem.tsx
+++ components/LibraryShowItem.tsx
@@ -5,10 +5,10 @@ import { Link } from "expo-router";
 const LibraryShowItem = ({ item, showType }) => {
   return (
     <Link href={`/posts/${item._id}`} asChild>
-      <Pressable className={"h-20 flex flex-row my-2 mx-4 space-x-3"}>
+      <Pressable className={"h-20 flex flex-row my-2 px-4 space-x-3"}>
         <View className={"h-20 w-20 bg-[#FFFFFF12]"}></View>
-        <View className={"flex justify-center"}>
-          <Text className={"text-white font-bold"}>
+        <View className={"flex justify-center flex-1"}>
+          <Text className={"text-white font-bold"} numberOfLines={2}>
             {item.text}
           </Text>
           {showType && <Text className={"text-[#B3B3B3]"}>{item.category}</Text>}
