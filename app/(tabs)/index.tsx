1a6cea9103df360c40b4108f389d19b52c17fff0
tunogya <20251942+tunogya@users.noreply.github.com>
2024-05-25 15:39:51 +0800

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,32 @@
-import { View, ScrollView } from "react-native";
+import {View, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { memo } from "react";
 import AddDreamButton from "../../components/AddButton";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store/store";
 
 function Page() {
   const insets = useSafeAreaInsets();
+  const { address } = useSelector((state: RootState) => state.user);
 
   return (
-    <View className={"flex flex-1 bg-[#121212]"}>
-      <ScrollView
-        scrollEventThrottle={1000}
-        showsVerticalScrollIndicator={false}
+    <View
+      className={"flex flex-1 h-full bg-[#121212]"}
+    >
+      <View
         style={{
           paddingTop: insets.top + 20,
-          paddingBottom: insets.bottom + 66,
         }}
       >
-      </ScrollView>
-      <AddDreamButton />
+        <View className={"p-4 flex flex-row justify-between items-center"}>
+          <Text className={"text-white font-bold text-2xl"}>
+            {address.slice(0, 6)}...{address.slice(-6)}
+          </Text>
+        </View>
+      </View>
+      <View className={"flex-1"}>
+      </View>
+      <AddDreamButton/>
     </View>
   );
 }
