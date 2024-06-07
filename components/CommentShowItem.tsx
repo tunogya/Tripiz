45edef161dc26dc4dd0c02f62f35b173e20710f2
tunogya <20251942+tunogya@users.noreply.github.com>
2024-06-07 19:37:17 +0800

    update

--- components/CommentShowItem.tsx
+++ components/CommentShowItem.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 import { Image } from "expo-image";
-import React, { FC, memo, useEffect, useState } from "react";
+import React, { FC, memo, useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import * as Crypto from "expo-crypto";
 import { selectPublicKey } from "../reducers/account/accountSlice";
+import { RootState } from "../store/store";
 
 export async function mapAddressToNumber(address: string) {
   const hash = await Crypto.digestStringAsync(
@@ -20,29 +21,36 @@ const CommentShowItem: FC<{
     pubkey: string;
     content: string;
     tags: [][];
+    tags_map: any;
     created_at: number;
   };
 }> = ({ item }) => {
-  const [number, setNumber] = useState<string | undefined>(undefined);
+  const [number, setNumber] = useState<number | undefined>(undefined);
+  const { avatar } = useSelector((state: RootState) => state.ui);
   const publicKey = useSelector(selectPublicKey);
 
   const fetchNumber = async (id: string) => {
     const data = await mapAddressToNumber(id);
-    setNumber(data.toString().padStart(4, "0"));
+    setNumber(data);
   };
 
   useEffect(() => {
     fetchNumber(item.pubkey);
   }, [item.pubkey]);
 
+  const username =
+    item.pubkey === publicKey
+      ? "Me"
+      : item?.tags_map?.username?.[0] || "Anonymous";
+
   return (
     <View className={"px-4 pt-4 flex flex-row space-x-3 bg-[#121212]"}>
-      {number ? (
+      {number !== undefined && item.pubkey && publicKey ? (
         <Image
           contentFit={"cover"}
           cachePolicy={"memory-disk"}
           source={{
-            uri: `https://www.larvalabs.com/cryptopunks/cryptopunk${(number || 0)?.toString().padStart(4, "0")}.png`,
+            uri: `https://www.larvalabs.com/cryptopunks/cryptopunk${(item.pubkey === publicKey ? avatar : number || 0)?.toString().padStart(4, "0")}.png`,
           }}
           className={"w-10 h-10 rounded-full bg-gray-400"}
         />
@@ -51,9 +59,7 @@ const CommentShowItem: FC<{
       )}
       <View className={"space-y-1.5 pb-4 flex-1 border-b border-[#FFFFFF12]"}>
         <View className={"flex flex-row justify-between items-end"}>
-          <Text className={"text-[#B3B3B3] text-[16px]"}>
-            {item.pubkey === publicKey ? "Me" : item.pubkey}
-          </Text>
+          <Text className={"text-[#B3B3B3] text-[16px]"}>{username}</Text>
           <Text className={"text-[#B3B3B3] text-xs"}>
             {new Date(item.created_at * 1000)
               .toLocaleDateString()
