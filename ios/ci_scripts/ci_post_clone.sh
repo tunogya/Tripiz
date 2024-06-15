c6050d7c785e97d7d4f914a7c7f5233bd8c5b3be
tunogya <tunogya@qq.com>
2024-06-16 01:38:15 +0800

    update

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,10 @@
 #!/bin/zsh
 
-echo "===== Installing CocoaPods ====="
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+# Install Node, CocoaPods, and yarn using Homebrew.
+brew install node
 brew install cocoapods
-echo "===== Installing Node.js ====="
-brew install node@20
-echo "===== Installing yarn ====="
 brew install yarn
 
 # Install dependencies
-echo "===== Running yarn install ====="
-yarn install
-echo "===== Running pod install ====="
-cd ios
+yarn
 pod install
