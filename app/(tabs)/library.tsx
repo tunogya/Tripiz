447fb3b35ceea63425c8d9b2053ce34412b2069e
tunogya <tunogya@qq.com>
2024-06-25 18:52:38 +0800

    update

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -7,7 +7,7 @@ import {
   ActivityIndicator,
   FlatList,
 } from "react-native";
-import { memo, useEffect, useMemo, useState } from "react";
+import React, { memo, useEffect, useMemo, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
@@ -20,6 +20,8 @@ import { useQuery } from "@realm/react";
 import { Event } from "../Event";
 import { useWebSocket } from "../../components/WebSocketProvider";
 import { uuid } from "expo-modules-core";
+import Svg, {Path} from "react-native-svg";
+import SearchForm from "../../components/SearchForm";
 
 const Page = () => {
   const insets = useSafeAreaInsets();
@@ -27,6 +29,7 @@ const Page = () => {
   const FILTERS = ["memories", "dreams", "reflections"];
   const [filter, setFilter] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [ showSearchForm, setShowSearchForm ] = useState(false);
   const publicKey = useSelector(selectPublicKey);
   const { send } = useWebSocket();
 
@@ -71,7 +74,7 @@ const Page = () => {
   }, [publicKey]);
 
   return (
-    <View className={"flex flex-1 bg-[#121212]"}>
+    <View className={"flex flex-1 bg-[#121212] relative"}>
       <View
         className={"shadow shadow-black bg-[#121212]"}
         style={{
@@ -94,6 +97,19 @@ const Page = () => {
             </Text>
           </View>
           <View className={"flex flex-row space-x-3 items-center"}>
+            <Pressable
+              className={"items-center justify-center flex h-10 w-10"}
+              onPress={() => {
+                setShowSearchForm(true);
+              }}
+            >
+              <Svg width={24} height={24} viewBox="0 0 24 24">
+                <Path
+                  fill={"white"}
+                  d="M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 0 1-2.077 5.816l4.344 4.344a1 1 0 0 1-1.414 1.414l-4.353-4.353a9.454 9.454 0 0 1-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z"
+                ></Path>
+              </Svg>
+            </Pressable>
             <Pressable
               className={"items-center justify-center flex h-10 w-10"}
               onPress={() => {
@@ -186,6 +202,11 @@ const Page = () => {
           )}
         />
       </View>
+      {
+        showSearchForm && (
+          <SearchForm onClose={() => setShowSearchForm(false)} />
+        )
+      }
     </View>
   );
 };
