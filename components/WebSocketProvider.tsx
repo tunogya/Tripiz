aace70089f5516d17cd4625dc8e93b461fd2a7d0
tunogya <tunogya@qq.com>
2024-06-29 02:07:47 +0800

    update

--- components/WebSocketProvider.tsx
+++ components/WebSocketProvider.tsx
@@ -23,6 +23,7 @@ const WebSocketProvider = ({ children }) => {
   };
 
   const connectWebSocket = () => {
+    console.log(url);
     ws.current = new WebSocket(url);
 
     ws.current.onopen = () => {
